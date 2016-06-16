82dd81b147abc46016bcf820e013b005a82738a6
shadog <medoasd19931@gmail.com>
2016-06-17 01:57:59 +0300

    Add files via upload

---
+++ plugin/rebot.lua
@@ -0,0 +1,28 @@
+﻿--[[
+#
+#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#:((
+# For More Information ....! 
+# manstir:  < @alasmr_199 > 
+
+# Version: 1.1
+#:))
+#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#
+]]
+do 
+local function run(msg, matches) 
+if ( msg.text ) then
+  if ( msg.to.type == "user" ) then
+     return "- Welcome to DevPoint !\n\n- For More Information Subscribe in \n\n- Channel : @DevPointTeam"
+    end 
+  end 
+end 
+return { 
+  patterns = { 
+     "(.*)$"
+  }, 
+  run = run 
+} 
+
+end 
